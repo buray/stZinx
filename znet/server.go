fd2036b528a5e17da54db07681e4d27ae7dd6ef0
buray <charlieburay@163.com>
2022-07-07 15:28:45 +0800

    添加路由

--- znet/server.go
+++ znet/server.go
@@ -17,13 +17,26 @@ type Server struct {
 	IP string
 	//服务绑定的端口
 	Port int
+	//当前Server由用户绑定的回调router,也就是Server注册的链接对应的处理业务
+	Router ziface.IRouter
 }
 
+/*
+  创建一个服务器句柄
+*/
+func NewServer(name string) ziface.IServer {
+	s := &Server{
+		Name:      name,
+		IPVersion: "tcp4",
+		IP:        "0.0.0.0",
+		Port:      7777,
+		Router:    nil,
+	}
+	return s
+}
 
-// ============== 实现 ziface.IServer 里的全部接口方法 ========
-
-
-// 开启网络服务
+//============== 实现 ziface.IServer 里的全部接口方法 ========
+//开启网络服务
 func (s *Server) Start() {
 	fmt.Printf("[START] Server listenner at IP: %s, Port %d, is starting\n", s.IP, s.Port)
 	//开启一个go去做服务端Linster业务
@@ -35,13 +48,16 @@ func (s *Server) Start() {
 			return
 		}
 		//2 监听服务器地址
-		listenner, err:= net.ListenTCP(s.IPVersion, addr)
+		listenner, err := net.ListenTCP(s.IPVersion, addr)
 		if err != nil {
 			fmt.Println("listen", s.IPVersion, "err", err)
 			return
 		}
 		//已经监听成功
 		fmt.Println("start Zinx server  ", s.Name, " succ, now listenning...")
+		//TODO server.go 应该有一个自动生成ID的方法
+		var cid uint32
+		cid = 0
 		//3 启动server网络连接业务
 		for {
 			//3.1 阻塞等待客户端建立连接请求
@@ -51,51 +67,29 @@ func (s *Server) Start() {
 				continue
 			}
 			//3.2 TODO Server.Start() 设置服务器最大连接控制,如果超过最大连接，那么则关闭此新的连接
-			//3.3 TODO Server.Start() 处理该新连接请求的 业务 方法， 此时应该有 handler 和 conn是绑定的
-			//我们这里暂时做一个最大512字节的回显服务
-			go func () {
-				//不断的循环从客户端获取数据
-				for  {
-					buf := make([]byte, 512)
-					cnt, err := conn.Read(buf)
-					if err != nil {
-						fmt.Println("recv buf err ", err)
-						break
-					}
-					//回显
-					if _, err := conn.Write(buf[:cnt]); err !=nil {
-						fmt.Println("write back buf err ", err)
-						continue
-					}
-				}
-			}()
+			//3.3 处理该新连接请求的 业务 方法， 此时应该有 handler 和 conn是绑定的
+			dealConn := NewConntion(conn, cid, s.Router)
+			cid++
+			//3.4 启动当前链接的处理业务
+			go dealConn.Start()
 		}
 	}()
 }
-
 func (s *Server) Stop() {
-	fmt.Println("[STOP] Zinx server , name " , s.Name)
-	// TODO  Server.Stop() 将其他需要清理的连接信息或者其他信息 也要一并停止或者清理
+	fmt.Println("[STOP] Zinx server , name ", s.Name)
+	//TODO  Server.Stop() 将其他需要清理的连接信息或者其他信息 也要一并停止或者清理
 }
-
 func (s *Server) Serve() {
 	s.Start()
-	// TODO Server.Serve() 是否在启动服务的时候 还要处理其他的事情呢 可以在这里添加
-	// 阻塞,否则主Go退出， listenner的go将会退出
+	//TODO Server.Serve() 是否在启动服务的时候 还要处理其他的事情呢 可以在这里添加
+	//阻塞,否则主Go退出， listenner的go将会退出
 	for {
-		time.Sleep(10*time.Second)
+		time.Sleep(10 * time.Second)
 	}
 }
 
-/*
-  创建一个服务器句柄
-*/
-func NewServer (name string) ziface.IServer {
-	s:= &Server {
-		Name :name,
-		IPVersion:"tcp4",
-		IP:"0.0.0.0",
-		Port:7777,
-	}
-	return s
+//路由功能：给当前服务注册一个路由业务方法，供客户端链接处理使用
+func (s *Server) AddRouter(router ziface.IRouter) {
+	s.Router = router
+	fmt.Println("Add Router succ! ")
 }
